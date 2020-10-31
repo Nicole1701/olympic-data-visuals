c3aa467c08ce01e302776bd6ca8217f23f4a86f1
Autzen <kristyskisfast@gmail.com>
2020-10-31 09:20:46 -0700

    minor changes

--- assets/js/sports.js
+++ assets/js/sports.js
@@ -1,26 +1,15 @@
 Highcharts.chart('sports-container', {
     chart: {
-        height: 1000,
-        // backgroundColor: '#D3D3D3',
+        height: 800,
+        width: 700,
         type: 'bar'
     },
-    // series: {
-    //     type: 'bar',
-    //     borderColor: '#FFFFFF'
-    // },
     title: {
         text: '120 Years of Olympic Sports',
-        style: {
-            fontSize: '30px'
-        }
-    },
-    subtitle: {
-        text: 'Source: <a href="https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results/home">Kaggle.com</a>',
         style: {
             fontSize: '20px'
         }
     },
-
     xAxis: {
         categories: [1896, 1900, 1904, 1906, 1908, 1912, 1920, 1924, 1928, 1932, 1936, 1948, 1952, 1956, 1960, 1964, 1968, 1972, 1976, 1980, 1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016],
         title: {
@@ -63,10 +52,13 @@ Highcharts.chart('sports-container', {
         layout: 'vertical',
         align: 'right',
         verticalAlign: 'top',
-        x: -40,
+        x: 0,
         y: 80,
         floating: true,
         borderWidth: 1,
+        itemStyle: {
+            fontSize: '18px'
+        },
         backgroundColor:
             Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
         shadow: true
